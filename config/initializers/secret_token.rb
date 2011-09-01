692fd79976bddc984c82f6f26b76bb0da2e3926f
Daniel Mejia <daniel.mvr1@gmail.com>
2011-09-01 08:07:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Explora::Application.config.secret_token = '0a0bfb5a1f4f088f480c4936829478571162a0e8be72993841759db61715dc87bda869d3cc610eead47cca6c5b40b830a99dc224f111034119c40a351bbf620d'
